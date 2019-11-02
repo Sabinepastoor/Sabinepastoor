ad5411cddfb55d4971871295ebc6f1f2e404c49c
Sabinepastoor <sabine.pastoor@hva.nl>
2019-11-02 18:26:00 +0100

    javascript & formulier

---
+++ script/script.js
@@ -0,0 +1,9 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+function change(){
+    var image = document.getElementById('Collage');
+    image.src = "img/Layout-100.jpg"
+}
