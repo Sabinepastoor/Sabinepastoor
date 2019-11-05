6f1514156b86c4406375e2c67ece6de94d1d4e64
Sabinepastoor <sabine.pastoor@hva.nl>
2019-11-05 17:47:23 +0100

    Nieuwe JS en hover

--- script/script.js
+++ script/script.js
@@ -7,4 +7,13 @@ function change(){
     var image = document.getElementById('Collage');
     image.src = "img/Layout-100.jpg"
 }
-
+// tutorial https://www.youtube.com/watch?v=hIRjlG-gbuI
+function insertText(){
+    var select = document.getElementById("subject_input"),
+        txtVal = document.getElementById("newValue").value,
+        newOptie = document.createElement ("option"),
+        newOptieVal = document.createTextNode(txtVal);
+// de nieuwe optie komt onder de opties die al bestaan in het dropdown menu
+    newOptie.appendChild(newOptieVal);
+    select.insertBefore(newOptie, select.lastChild);
+}
